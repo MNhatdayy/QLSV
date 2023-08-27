be0e65f0abec8deb031559c51d4c934876c24870
Admin <Admin@MNHAT>
2023-08-27 10:42:30 +0700

    Add project files.

---
+++ Nguoi.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+namespace QLSV
+{
+    internal class Nguoi
+    {
+        public string hoten;
+        public int age;
+        public Nguoi()
+        {
+        }
+        public Nguoi(string hoten, int age)
+        {
+            this.hoten = hoten;
+            this.age = age;
+        }
+        string getHoten()
+        {
+            return hoten;
+        }
+        public int getAge()
+        {
+            return age;
+        }
+        public void setHoten(string hoten)
+        {
+            this.hoten = hoten;
+        }
+        public void setAge(int age)
+        {
+            this.age = age;
+        }
+        public void Input()
+        {
+            Console.WriteLine("Nhap ho ten: ");
+            hoten = string.(Console.ReadLine());
+            Console.WriteLine("Nhap tuoi: ");
+            Console.ReadLine();
+        }
+        public void Output()
+        {
+            Console.WriteLine(hoten + " | " + age);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace QLSV
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Nguoi n1 = new Nguoi();
+            n1.Input();
+            n1.Output
+        }
+    }
+}
